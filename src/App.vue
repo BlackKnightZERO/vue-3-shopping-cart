4bfbeba31347104651e34a96b9c37396cc44bd84
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-02 16:56:36 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
+
+  <header class="top-bar spread">
+      <nav class="top-bar-nav">
+        <router-link to="/" class="top-bar-link">
+          <i class="icofont-spoon-and-fork"></i>
+          <span>Home</span>
+        </router-link>
+        <router-link to="/products" class="top-bar-link">
+          <span>Products</span>
+        </router-link>
+        <router-link to="past-orders" class="top-bar-link">
+          <span>Past Orders</span>
+        </router-link>
+      </nav>
+      <!--
+      <router-link to="#" class="top-bar-cart-link" @click="toggleSideBarCart">
+        <i class="icofont-cart-alt icofont-1x"></i>
+        <span id="nav-cart-item-count">Cart ({{ cartItemCount }})</span>
+      </router-link>
+      -->
+  </header>
+  <!--
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/products">Products</router-link>
   </div>
+  -->
   <router-view/>
 </template>
 
