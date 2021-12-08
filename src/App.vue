9f69c1ae5c4df5cf404d6976722586e7a04a068d
Arif Faysal <ayon.arif.10@gmail.com>
2021-12-07 22:13:04 -0500

    updated sidebar, products, home page

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,62 @@
           <span>Past Orders</span>
         </router-link>
       </nav>
-      <!--
-      <router-link to="#" class="top-bar-cart-link" @click="toggleSideBarCart">
+      
+      <div to="#" class="top-bar-cart-link" @click="toggleSideBarCart">
         <i class="icofont-cart-alt icofont-1x"></i>
         <span id="nav-cart-item-count">Cart ({{ cartItemCount }})</span>
-      </router-link>
-      -->
+      </div>
+      
   </header>
-  <router-view/>
+  <router-view :inventory="inventory" />
+  <SideBar
+    v-if="isSideBarCartVisible" 
+    :toggle="toggleSideBarCart"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeCartItem" />
 </template>
+
+<script>
+import SideBar from '@/components/SideBar.vue'
+import food from '@/food.json'
+
+
+export default {
+  components: {
+    SideBar
+  },
+  data() {
+    return {
+      isSideBarCartVisible: false,
+      totalCartItemType:0,
+      inventory: food,
+      cart:{},
+    }
+  },
+  computed:{
+    cartItemCount:{
+      get() {
+        return Object.values(this.cart).reduce((acc,curr,index) => {
+          return acc + curr
+        }, 0)
+      }
+    }
+  },
+  methods: {
+    addToCart(name, idx) {
+        if( !this.cart[name] ) this.cart[name] = 0
+        this.cart[name] += this.inventory[idx].quantity
+        this.inventory[idx].quantity = 0
+        console.log(this.cart)
+      },
+      toggleSideBarCart(){
+        this.isSideBarCartVisible = !this.isSideBarCartVisible
+      },
+      removeCartItem(name) {
+        delete this.cart[name]
+      }
+  },
+}
+</script>
+
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,64 @@
 <template>
   <div>
-    <h1>This is Products page</h1>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <div class="card-container">
+
+          <div class="card" v-for="(item, idx) in inventory" :key="item.id">
+            <div class="card-title">
+              {{ item.name }}
+            </div>
+            <div class="card-body">
+              <i class="icofont-10x icofont-{{ item.icon }}"></i>
+              <form>
+                <div class="row">
+                  <div class="cell">
+                    <label>Type:</label>
+                  </div>
+                  <div class="cell">
+                    <em>{{ item.type }}</em>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="cell">
+                    <label>Price:</label>
+                  </div>
+                  <div class="cell">
+                    ${{ item.price.USD }}
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="cell">
+                    <label>Quantity:</label>
+                  </div>
+                  <div class="cell">
+                    <input type="number" value="0">
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-light">Add to cart</button>
+            </div>
+          </div>
+
+        </div>
+      </main>
+        <footer class="footer-class">
+          vue 3
+        </footer>
   </div>
 </template>
+
+<script>
+import food from '../food.json'
+export default {
+  name: 'Products',
+  components: {},
+  data () {
+  	return {
+  		inventory: food
+  	} 
+  },
+}
+</script>
